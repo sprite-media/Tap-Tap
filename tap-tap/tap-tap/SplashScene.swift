bb04aa714d24e8bd4e08d8a9174d38e2245f5348
Heaseo Chung <look0713@hotmail.com>
2020-03-19 17:20:39 -0400

    Update SplashScene.swift

--- tap-tap/tap-tap/SplashScene.swift
+++ tap-tap/tap-tap/SplashScene.swift
@@ -17,45 +17,43 @@ class SplashScene : SKScene
     
     override func didMove(to view: SKView)
     {
-       CreateTeamLogo()
+        self.backgroundColor = SKColor.white
+        CreateSwiftAndSpriteKitLogo()
     }
     
    func CreateTeamLogo()
    {
         let teamLogoNode = SKSpriteNode(texture: SKTexture(imageNamed: "logo_spritemedia"))
-        teamLogoNode.size = self.frame.size
+        teamLogoNode.size.width = self.frame.size.width
+        teamLogoNode.size.height = self.frame.size.height/2
         teamLogoNode.position = CGPoint(x: self.frame.size.width/2, y: self.frame.size.height/2)
         teamLogoNode.alpha = 0
         let teamLogoAction =   SKAction.sequence([SKAction.fadeIn(withDuration: 2.0), SKAction.fadeOut(withDuration: 2.0)])
         self.addChild(teamLogoNode)
     
-        teamLogoNode.run(teamLogoAction, completion: self.CreateSwiftLogo)
+        teamLogoNode.run(teamLogoAction, completion: self.ChangeScene)
     }
-    func CreateSwiftLogo()
+    func CreateSwiftAndSpriteKitLogo()
     {
+        let logos = SKNode()
+        logos.position = CGPoint(x: self.frame.size.width/2, y: self.frame.size.height/2)
+        logos.alpha = 0
+        self.addChild(logos)
+        
         let swiftLogoNode = SKSpriteNode(texture: SKTexture(imageNamed: "logo_swift"))
-             swiftLogoNode.size.width = self.frame.size.width/2
+        swiftLogoNode.size.width = self.frame.size.width/1.5
              swiftLogoNode.size.height = self.frame.size.height/3
-             swiftLogoNode.position = CGPoint(x: self.frame.size.width/2, y: self.frame.size.height/2)
-             swiftLogoNode.alpha = 0
-             let swiftLogoAction = SKAction.sequence([SKAction.fadeIn(withDuration: 2.0), SKAction.fadeOut(withDuration: 2.0)])
-             swiftLogoNode.run(swiftLogoAction)
-        self.addChild(swiftLogoNode)
+             swiftLogoNode.position = CGPoint(x: 0, y: self.frame.height/5)
+        logos.addChild(swiftLogoNode)
         
-        swiftLogoNode.run(swiftLogoAction, completion: self.CreateSpriteKitLogo)
-    }
-    func CreateSpriteKitLogo()
-    {
         let spriteKitLogoNode = SKSpriteNode(texture: SKTexture(imageNamed: "logo_spritekit"))
-        spriteKitLogoNode.size.width = self.frame.size.width/2
+        spriteKitLogoNode.size.width = self.frame.size.width/1.5
         spriteKitLogoNode.size.height = self.frame.size.height/3
-        spriteKitLogoNode.position = CGPoint(x: self.frame.size.width/2, y: self.frame.size.height/2)
-        spriteKitLogoNode.alpha = 0
-        let spriteKitLogoAction = SKAction.sequence([SKAction.fadeIn(withDuration: 2.0), SKAction.fadeOut(withDuration: 2.0)])
-        self.addChild(spriteKitLogoNode)
+        spriteKitLogoNode.position = CGPoint(x: 0, y: -self.frame.height/5)
+        logos.addChild(spriteKitLogoNode)
         
-        spriteKitLogoNode.run(spriteKitLogoAction, completion: ChangeScene)
-
+        let logosAction = SKAction.sequence([SKAction.fadeIn(withDuration: 2.0), SKAction.fadeOut(withDuration: 2.0)])
+        logos.run(logosAction, completion: self.CreateTeamLogo)
     }
     func ChangeScene()
     {
