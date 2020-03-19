23d896a7986cbfb9d00175ff6dd3db907f58a7c5
BJin2 <bomjan@naver.com>
2020-03-19 14:10:31 -0400

    created skeleton class

---
+++ tap-tap/tap-tap/SettingScreen.swift
@@ -0,0 +1,14 @@
+//
+//  SettingScreen.swift
+//  tap-tap
+//
+//  Created by SpriteMedia on 3/19/20.
+//  Copyright © 2020 SpriteMedia. All rights reserved.
+//
+
+import SpriteKit
+
+class SettingScreen
+{
+    
+}
---
+++ tap-tap/tap-tap/Slider.swift
@@ -0,0 +1,14 @@
+//
+//  Slider.swift
+//  tap-tap
+//
+//  Created by SpriteMedia on 3/19/20.
+//  Copyright © 2020 SpriteMedia. All rights reserved.
+//
+
+import SpriteKit
+
+class Slider
+{
+    
+}
