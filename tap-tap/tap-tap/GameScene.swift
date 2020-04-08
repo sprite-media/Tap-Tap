5e91c75cfd03a06e4449250d5e31df5d42ce8730
Hyukin Kown <901po3@gmail.com>
2020-04-07 22:41:13 -0400

    1

--- tap-tap/tap-tap/GameScene.swift
+++ tap-tap/tap-tap/GameScene.swift
@@ -12,9 +12,14 @@ import os.log
 
 class GameScene: SKScene
 {
+    //BG
+    var bg : SKNode?
+    var timerBg : SKNode?
+    var goalBg : SKNode?
+    
     //Timer
-    var timerBar : SKSpriteNode = SKSpriteNode()
-    var timerBarBG : SKSpriteNode = SKSpriteNode()
+    var timerBar : SKNode?
+    var timerBarBg : SKNode?
     var time = 10.0
     
     // Main Images
@@ -23,7 +28,6 @@ class GameScene: SKScene
 
     
     // Goal
-    var goalBG = SKSpriteNode()
     var goalNum : Int = 0
     var goalArray = [Int]()
     var goalImageContainer = [SKSpriteNode]()
@@ -33,13 +37,20 @@ class GameScene: SKScene
     var previousTimeInterval : TimeInterval = 0
     
     //Setting
-    var settingButton : SKSpriteNode = SKSpriteNode()
+    var settingButton : SKNode?
     var setting : SettingScreen?
     
     var BGM : SKAudioNode = SKAudioNode()
     
     override func didMove(to view: SKView) {
         //initialize nodes
+        bg = childNode(withName: "bg")
+        timerBg = bg?.childNode(withName: "timerBg")
+        goalBg = bg?.childNode(withName: "goalBg")
+        timerBar = timerBg?.childNode(withName: "timerBar")
+        timerBarBg = timerBg?.childNode(withName: "timerBarBg")
+        settingButton = timerBg?.childNode(withName: "settingButton")
+        
         //temp for m2 only
         toWin = SKSpriteNode(texture: SKTexture(imageNamed: "CIRCLE"))
         toWin.scale(to: CGSize(width: WIDTH*0.2, height: WIDTH*0.2))
@@ -53,7 +64,6 @@ class GameScene: SKScene
         toLose.zPosition = 5
         addChild(toLose)
         
-        CreateUI()
         GameTimer()
         CreateGoal()
         setting = SettingScreen(_parent: self)
@@ -79,54 +89,6 @@ class GameScene: SKScene
 //MARK: Initialization
 extension GameScene {
     
-    func CreateUI()
-    {
-        let bg = SKSpriteNode(texture: SKTexture(imageNamed: "bg_big_yellow"))
-        bg.scale(to: CGSize(width: WIDTH, height: HEIGHT))
-        bg.position = CGPoint(x : 0, y: 0)
-        bg.zPosition = -1
-        addChild(bg)
-        
-        let timerBG : SKSpriteNode = SKSpriteNode(texture: SKTexture(imageNamed: "CasualUI_15_2"))
-        timerBG.scale(to: CGSize(width: WIDTH, height: HEIGHT * 0.2))
-        timerBG.position = CGPoint(x : 0, y : (timerBG.size.height * 0.5))
-        timerBG.zPosition = 2
-        addChild(timerBG)
-        
-        let timerFrame : SKSpriteNode = SKSpriteNode(texture: SKTexture(imageNamed: "CasualUI_5_4"))
-        timerFrame.scale(to: CGSize(width: WIDTH, height: HEIGHT * 0.1))
-        timerFrame.position = CGPoint(x : 0, y : timerBG.position.y - (timerBG.size.height*0.5) + (timerFrame.size.height*0.5))
-        timerFrame.zPosition = 5
-        addChild(timerFrame)
-        
-        timerBar = SKSpriteNode(texture: SKTexture(imageNamed: "CasualUI_5_3"))
-        timerBar.scale(to: CGSize(width: timerFrame.size.width * 0.96, height: timerFrame.size.height*0.9))
-        timerBar.position = CGPoint(x: 0.03, y: timerFrame.position.y)
-        timerBar.zPosition = 4
-        timerBar.anchorPoint = CGPoint(x: 0.0, y: 0.5)
-        addChild(timerBar)
-        
-        timerBarBG = SKSpriteNode(texture: SKTexture(imageNamed: "CasualUI_5_2"))
-        timerBarBG.scale(to: CGSize(width: timerFrame.size.width * 0.96, height: timerFrame.size.height*0.9))
-        timerBarBG.position = timerFrame.position
-        timerBarBG.zPosition = 3
-        addChild(timerBarBG)
-                	
-        goalBG = SKSpriteNode(texture: SKTexture(imageNamed: "CasualUI_15_2"))
-        goalBG.scale(to: CGSize(width: WIDTH, height: HEIGHT*0.2))
-        goalBG.position = CGPoint(x : 0.5, y: goalBG.size.height*0.5)
-        goalBG.zPosition = 1
-        addChild(goalBG)
-        
-        settingButton = SKSpriteNode(texture : SKTexture(imageNamed: "CasualUI_6_5"))
-        settingButton.scale(to: CGSize(width: WIDTH*0.1, height: WIDTH * 0.1))
-        settingButton.position = CGPoint(x: 0.1, y: HEIGHT - (WIDTH*0.1))
-        settingButton.zPosition = 10
-        settingButton.color = SKColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)
-        settingButton.colorBlendFactor = 1.0
-        addChild(settingButton)
-    }
-    
     func CreateGoal() {
         
         //Set number of Images to Draw
@@ -151,9 +113,9 @@ extension GameScene {
             let goalImage = SKSpriteNode(texture: SKTexture(imageNamed: appleNameArray[pickedAppleIndex]))
             appleNameArray.remove(at: pickedAppleIndex)
             
-            goalBG.addChild(goalImage)
-            goalImage.scale(to: CGSize(width: goalBG.size.width / 11, height: goalBG.size.width / 11))
-            goalImage.position = CGPoint(x: goalBG.size.width/8 * CGFloat(n) - goalBG.size.width/3 , y: -goalBG.size.height/10)
+            goalBg?.addChild(goalImage)
+            goalImage.scale(to: CGSize(width: goalBg!.xScale / 11, height: goalBg!.xScale / 11))
+            goalImage.position = CGPoint(x: goalBg!.xScale / 8 * CGFloat(n) - goalBg!.xScale / 3 , y: -goalBg!.yScale / 10)
             goalImage.zPosition = 5
             goalImageContainer.append(goalImage)
 
@@ -168,7 +130,7 @@ extension GameScene {
     func GameTimer() {
         
         let reduce = SKAction.scale(to: CGSize(width: 0.0, height: HEIGHT * 0.09), duration: self.time)
-        timerBar.run(reduce)
+        timerBar?.run(reduce)
         
         Timer.scheduledTimer(withTimeInterval: self.time, repeats: false) { (timer) in
             os_log("time's up")
@@ -206,23 +168,23 @@ extension GameScene {
        {
            for t in touches
            {
-               if(settingButton.contains(t.location(in: self)))
+               if(settingButton!.contains(t.location(in: self)))
                {
-                   setting!.Show(visible: true)
-                   settingButton.removeFromParent()
+                    setting!.Show(visible: true)
+                    settingButton!.removeFromParent()
                }
                else if(setting!.back.contains(t.location(in: self)))
                {
-                   setting!.Show(visible: false)
-                   addChild(settingButton)
+                    setting!.Show(visible: false)
+                    addChild(settingButton!)
                }
                else if(toWin.contains(t.location(in: self)))
                {
-                   Win()
+                    Win()
                }
                else if(toLose.contains(t.location(in: self)))
                {
-                   Lose()
+                    Lose()
                }
                setting!.slider_bgm!.ValueChange(touchPoint: t.location(in: self), function : ChangeBGMVolume)
                setting!.slider_sfx!.ValueChange(touchPoint: t.location(in: self), function : ChangeSFXVolume)
