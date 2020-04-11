f09212e5d2cbb0cdcacfe023f146fc1c9b5326e4
Hyukin Kown <901po3@gmail.com>
2020-04-10 22:50:45 -0400

    1

--- tap-tap/tap-tap/GameoverScene.swift
+++ tap-tap/tap-tap/GameoverScene.swift
@@ -47,7 +47,7 @@ class GameoverScene : SKScene
         okButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 10, right: 0)
         
         okButton.addTarget(self, action: #selector(OKButton), for: .touchUpInside)
-        okButton.frame = CGRect(x:  (okImage?.size.width)!/2, y: 550, width: (okImage?.size.width)!, height: (okImage?.size.height)!)
+        okButton.frame = CGRect(x:  (okImage?.size.width)!/2, y: frame.height / 3.5, width: (okImage?.size.width)!, height: (okImage?.size.height)!)
            self.view?.addSubview(okButton)
         okButton.isHidden = false
     }
